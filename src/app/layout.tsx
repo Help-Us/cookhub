ccbe42ba299e0c49982c682638cf0ce1259d91a3
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-03-15 18:39:55 +0900

    Revert "[feat] - 메인 페이지 레이아웃 구현 #1"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div>TEST</div>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
