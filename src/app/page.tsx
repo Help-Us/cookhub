6a65c860991c916ecfde942849a9848e1961d648
limhyerin <rin091345@gmail.com>
2024-03-18 20:23:20 +0900

    feat: 검색창 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Main Page</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-[#F5EEE6]">
+        <div className="w-full max-w-4xl h-[500px] flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold mb-4">레시피 검색</h1>
+          <input type="text" placeholder="레시피를 입력해주세요" 
+            className="form-input px-4 py-2 w-full max-w-md border rounded shadow" />
+        </div>
+      </div>
+    </>
+  )
 }
