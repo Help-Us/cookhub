4d46b51667005712ddc5520251055c15d063bdbb
leejieun2023 <lge9380@naver.com>
2024-03-19 10:11:38 +0900

    [feat:#8] - 음식 이름 컴포넌트 틀 잡기

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Ingredients from "@/components/detail/Ingredients";
 import Nutrient from "@/components/detail/Nutrient";
+import FoodName from "@/components/detail/FoodName";
 
 const DetailPage = () => {
     return (
         <main className="flex justify-center items-center">
             <div className="mr-5">
+                <FoodName />
                 <Ingredients />
             </div>
             <div className="">
---
+++ src/components/detail/FoodName.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FoodName = () => {
+
+    return (
+        <div className="mb-10">
+            <p className="text-brown text-2xl font-bold mb-2.5">음식이름</p>
+
+            <div className="bg-grey h-10 w-28 border-pink border-2 rounded-3xl">
+
+            </div>
+        </div>
+    );
+}
+
+export default FoodName;
