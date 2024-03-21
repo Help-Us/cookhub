fd7e0fdeb4e248125b87b016ababc700b43b26a2
Jiwon <tj1tj2tj3@naver.com>
2024-03-21 19:14:12 +0900

    #27 헤더 스타일링

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,10 +4,18 @@ import React from "react";
 export default function Navigation() {
   return (
     <nav>
-      <Link href={"/"}>Home </Link>
-      <Link href={"/login"}>Login </Link>
-      <Link href={"/mypage"}>MyPage </Link>
-      <Link href={"/category/All"}>카테고리</Link>
+      <div className="flex justify-between py-4 px-8 text-[color:var(--titleColor)] border-b border-solid border-[color:var(--titleColor)]">
+        <Link href={"/category/All"}>카테고리별 검색</Link>
+        <Link href={"/"} className="text-lg">
+          COOKHUB
+        </Link>
+        <div>
+          <Link href={"/mypage"} className="mr-5">
+            마이페이지
+          </Link>
+          <Link href={"/login"}>로그인</Link>
+        </div>
+      </div>
     </nav>
   );
 }
