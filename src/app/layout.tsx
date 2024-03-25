af4c382a9f4c78ecc52829f2ae0792180afa20a6
Jiwon <tj1tj2tj3@naver.com>
2024-03-25 16:11:07 +0900

    [style] css 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./provider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Navigation from "@/components/layout/Navigation";
 import Footer from "@/components/layout/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Cookhub",
   description: "Discover and share delightful recipes."
 };
+
+const noto = Noto_Sans_KR({
+  subsets: ["latin"] // 또는 preload: false
+});
 
 export default function RootLayout({
   children
@@ -20,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={noto.className}>
         <Navigation />
         <QueryProvider>
           {children}
